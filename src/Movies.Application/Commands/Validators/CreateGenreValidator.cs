bcc36ca674a462ae52a2437a3d4295d65273d209
AccountsStatements <edward_el_doc@hotmail.com>
2025-09-19 03:46:22 -0600

    Modified Genres Controller and property name of CreateGenreCommand

--- src/Movies.Application/Commands/Validators/CreateGenreValidator.cs
+++ src/Movies.Application/Commands/Validators/CreateGenreValidator.cs
@@ -6,7 +6,7 @@ namespace Movies.Application.Commands.Validators
     {
         public CreateGenreValidator()
         {
-            RuleFor(x => x.name)
+            RuleFor(x => x.genreName)
                 .NotEmpty().WithMessage("Genre name must not be empty.")
                 .MaximumLength(50).WithMessage("Genre name must not exceed 50 characters.");
         }
---
+++ src/Movies.Application/Queries/GetGenreByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Movies.Domain.Entities;
+
+namespace Movies.Application.Queries
+{
+    public record GetGenreByIdQuery(int Id) : IRequest<Genre?>;
+}
