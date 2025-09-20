a32ea7d937c4e4fb10716fdd274ef123e6060b50
AccountsStatements <edward_el_doc@hotmail.com>
2025-09-19 23:24:33 -0600

    Added validation

--- src/Movies.Application/Commands/Validators/CreateGenreValidator.cs
+++ src/Movies.Application/Commands/Validators/CreateGenreValidator.cs
@@ -7,8 +7,16 @@ namespace Movies.Application.Commands.Validators
         public CreateGenreValidator()
         {
             RuleFor(x => x.genreName)
-                .NotEmpty().WithMessage("Genre name must not be empty.")
-                .MaximumLength(50).WithMessage("Genre name must not exceed 50 characters.");
+                .NotEmpty().WithMessage("{PropertyName} must not be empty.")
+                .Must(StartsWithUppercase).WithMessage("{PropertyName} must start with an uppercase letter.")
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+        }
+
+        private bool StartsWithUppercase(string genreName)
+        {
+            if (string.IsNullOrEmpty(genreName))
+                return false;
+            return char.IsUpper(genreName[0]);
         }
     }
 }
